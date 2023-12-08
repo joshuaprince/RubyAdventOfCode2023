46fc01f4c74e3409bb905c39347a90b740c5eb48
Joshua Prince <joshua@jtprince.com>
2023-12-07 19:31:59 -0800

    Day 7

---
+++ day7/day7-pt1.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+in_file = './day7/day7.txt'
+
+class Hand
+  attr_reader :str, :ranking, :bid, :score
+
+  def initialize(str, ranking, bid, score)
+    @str = str
+    @ranking = ranking
+    @bid = bid
+    @score = score
+  end
+end
+
+FIVE_KIND = 7
+FOUR_KIND = 6
+FULL_HOUSE = 5
+THREE_KIND = 4
+TWO_PAIR = 3
+ONE_PAIR = 2
+HIGH = 1
+
+hands = IO.foreach(in_file).map do |line|
+  m = /(\w{5}) (\d+)/.match(line)
+  str = m[1]
+  bid = Integer(m[2])
+
+  counts = str.split('').map { |card| str.count(card) }.sort.reverse
+  # print counts
+
+  if counts[0] == 5
+    ranking = FIVE_KIND
+  elsif counts[0] == 4
+    ranking = FOUR_KIND
+  elsif (counts[0] == 3) && (counts[3] == 2)
+    ranking = FULL_HOUSE
+  elsif counts[0] == 3
+    ranking = THREE_KIND
+  elsif (counts[0] == 2) && (counts[2] == 2)
+    ranking = TWO_PAIR
+  elsif counts[0] == 2
+    ranking = ONE_PAIR
+  elsif counts[0] == 1
+    ranking = HIGH
+  else
+    throw "missing hand #{str}"
+  end
+
+  # effective score
+  score = ranking
+  str.split('').each do |card|
+    value = case card
+    when 'A'
+      14
+    when 'K'
+      13
+    when 'Q'
+      12
+    when 'J'
+      11
+    when 'T'
+      10
+    else
+      Integer(card)
+    end
+    score = score * 15 + value
+  end
+
+  Hand.new(str, ranking, bid, score)
+end
+
+sorted_hands = hands.sort_by(&:score)
+
+puts(sorted_hands.each_with_index.sum { |h, idx| h.bid * (idx + 1) })
+
+# puts sorted_hands.map { |h| "#{h.str} (#{h.score})" }
